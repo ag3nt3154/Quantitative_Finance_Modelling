74df477444f3b278e2bfa0907d515085fa85bdb7
ag3nt3154 <ag3nt3154@gmail.com>
2023-03-07 15:30:44 +0800

    update

---
+++ Pairs Trading/misc.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from datetime import datetime
+import yahoo_fin.stock_info as si
+
+
+def get_price_data(ticker, save=True):
+    # Get historical price data from yahoo_fin
+    df = si.get_data(ticker)
+    # Change date from index to column
+    df.reset_index(inplace=True)
+    # Change column name to 'date'
+    df = df.rename(columns={'index': 'date'})
+    if save:
+        # Save to csv
+        df.to_csv(ticker + '.csv')
+    return(df)
+
+
+def del_unnamed_col(dataframe):
+    '''
+    Delete any column with header 'Unnamed'
+    '''
+    dataframe.drop(dataframe.filter(regex='Unnamed').columns, axis=1, inplace=True)
+    return dataframe
+
+def str2date(date_string):
+    '''
+    Change date in string format to datetime format
+    '''
+    if '-' in date_string:
+        datetime_object = datetime.strptime(date_string, '%Y-%m-%d')
+    elif '/' in date_string:
+        datetime_object = datetime.strptime(date_string, '%d/%m/%Y')
+    
+    return pd.to_datetime(datetime_object)
+
+def clean_df(dataframe):
+    '''
+    Clean up dataframe from csv. Delete 'Unnamed' columns and convert dates from string format to datetime format.
+    '''
+    dataframe = del_unnamed_col(dataframe)
+    dataframe.reset_index(drop=True)
+    dataframe['date'] = dataframe.apply(lambda x: str2date(x['date']), axis=1)
+    return dataframe
+
+
+def date2str(datetime_obj):
+    day = datetime_obj.day
+    month = datetime_obj.month
+    year = datetime_obj.year
+    
+    return str(day) + '/' + str(month) + '/' + str(year)
