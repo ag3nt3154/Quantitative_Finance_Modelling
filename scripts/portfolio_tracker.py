3d5f2cf72e52988991235cb436b6a95dc9914aad
ag3nt3154 <ag3nt3154@gmail.com>
2025-01-26 23:22:04 +0800

    update

--- scripts/portfolio_tracker.py
+++ scripts/portfolio_tracker.py
@@ -1,11 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 class PortfolioTracker:
     def __init__(self, tickers):
         self.df = pd.DataFrame()
         self.tickers = tickers
         self.dates = []
         self.portfolio_value = []
-        self.asset_values = {ticker: [] for ticker in self.tickers}
-        self.asset_pnl = {ticker: [] for ticker in self.tickers}
+        self.asset_values = []
+        self.asset_pnl = []
 
 
     def next(
@@ -13,8 +17,19 @@ class PortfolioTracker:
         date,
         portfolio_value: float,
         asset_values: dict, 
-        asset_weights: dict, 
         asset_pnl: dict, 
-        cash: float,
     ):
         self.dates.append(date)
+        self.portfolio_value.append(portfolio_value)
+        self.asset_values.append(asset_values)
+        self.asset_pnl.append(asset_pnl)
+
+    def build_df(self):
+        plt.plot(self.portfolio_value)
+        plt.show()
+        self.df['date'] = self.dates
+        self.df['portfolio_value'] = self.portfolio_value
+        for ticker in self.tickers:
+            self.df['asset_values_' + ticker] = [f[ticker] for f in self.asset_values]
+            self.df['asset_pnl_' + ticker] = [f[ticker] for f in self.asset_pnl]
+        
